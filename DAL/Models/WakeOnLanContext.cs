b3c6e306fc33bea55c8547b29539353bc8f73a05
Santiago Alvarado <s.alvarado0@icloud.com>
2020-07-26 17:24:42 -0300

    test

--- DAL/Models/WakeOnLanContext.cs
+++ DAL/Models/WakeOnLanContext.cs
@@ -20,7 +20,7 @@ namespace DAL.Models
             if (!optionsBuilder.IsConfigured)
             {
                 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Database=WakeOnLan");
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=WakeOnLan;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;");
             }
         }
 
